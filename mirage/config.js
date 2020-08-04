f90cd05aaa73472c21e13feb2200e4c879f485ff
John Costanzo <jrock2004@gmail.com>
2020-08-04 15:35:27 -0400

    Updating mirage config

--- mirage/config.js
+++ mirage/config.js
@@ -56,8 +56,9 @@ export default function () {
           id: 1,
           type: "menu-item-service",
           attributes: {
-            name: "Strictly Bald",
             duration: "15 min",
+            itemId: 124,
+            name: "Strictly Bald",
             price: "$ 20.00",
           },
         },
@@ -65,8 +66,9 @@ export default function () {
           id: 2,
           type: "menu-item-package",
           attributes: {
-            name: "Mani Pendi",
             duration: "45 min - 1hr 24 min",
+            itemId: 20,
+            name: "Mani Pendi",
             price: "$ 120.00",
           },
         },
@@ -74,8 +76,9 @@ export default function () {
           id: 3,
           type: "menu-item-service",
           attributes: {
-            name: "60 Minute Massage",
             duration: "1 hr",
+            itemId: 202,
+            name: "60 Minute Massage",
             price: "$ 75.00",
           },
         },
@@ -83,8 +86,9 @@ export default function () {
           id: 4,
           type: "menu-item-service",
           attributes: {
-            name: "Facial",
             duration: "1hr 15 min",
+            itemId: 2021,
+            name: "Facial",
             price: "$ 50.00",
           },
         },
@@ -92,36 +96,13 @@ export default function () {
           id: 5,
           type: "menu-item-service",
           attributes: {
-            name: "Pendicure",
             duration: "45 min",
+            itemId: 20211,
+            name: "Pendicure",
             price: "$ 20.00",
           },
         },
       ],
     };
   });
-
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
 }
